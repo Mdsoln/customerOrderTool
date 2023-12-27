5c83466f62437bdde783f64531d505af7e6c8558
MUDDY FAKIH <muddyfakih98@gmail.com>
2023-12-27 17:38:10 +0300

    implementing simple mail sender from Java mail sender

--- src/main/java/com/customerordersystem/service/impl/EmailService.java
+++ src/main/java/com/customerordersystem/service/impl/EmailService.java
@@ -1,2 +1,23 @@
-package com.customerordersystem.service.impl;public class EmailService {
+package com.customerordersystem.service.impl;
+
+import com.customerordersystem.dto.CustomerRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public void SendAlertToSupplierAfterOrders(CustomerRequest customerRequest){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setSubject("Customer Orders:");
+        mailMessage.setTo("supplierEmail@gmail.com");
+        mailMessage.setText("Customer: "+customerRequest.getCustomerName()+ " with email "+
+              customerRequest.getCustomerEmail() + " has ordered product "+customerRequest.getProductName());
+
+        javaMailSender.send(mailMessage);
+    }
 }
