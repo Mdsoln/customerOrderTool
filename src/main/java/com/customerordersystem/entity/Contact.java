fe29c4f0b60cd1ccf8406a7ed778fdf5d9095b2d
MUDDY FAKIH <muddyfakih98@gmail.com>
2023-12-27 17:39:33 +0300

    classs for holding the phone numbers of suppliers and customers

--- src/main/java/com/customerordersystem/entity/Contact.java
+++ src/main/java/com/customerordersystem/entity/Contact.java
@@ -1,2 +1,28 @@
-package com.customerordersystem.entity;public class Contact {
+package com.customerordersystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "contact")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long contact_id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id",referencedColumnName = "id")
+    private List<Customer> customer;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name = "supplier_id",referencedColumnName = "id")
+    private List<Supplier> supplier;
 }
