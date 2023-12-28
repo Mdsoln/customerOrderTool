e728151725e08984df55c6c096729a05cbd61e16
MUDDY FAKIH <muddyfakih98@gmail.com>
2023-12-28 23:52:40 +0300

    securing of api and modified accesses to apis

--- src/main/java/com/customerordersystem/config/SecurityConfig.java
+++ src/main/java/com/customerordersystem/config/SecurityConfig.java
@@ -1,2 +1,65 @@
-package com.customerordersystem.config;public class SecurityConfig {
+package com.customerordersystem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Autowired
+    public UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests((authorise)->
+                        authorise
+                                .requestMatchers("")
+                                .permitAll()
+                ).formLogin(
+                        form->
+                                form
+                                        .loginPage("/login")
+                                        .loginProcessingUrl("/login")
+                                        .defaultSuccessUrl("/home")
+                                        .permitAll()
+                ).logout(
+                        logout->
+                                logout
+                                        .invalidateHttpSession(true)
+                                        .clearAuthentication(true)
+                                        .invalidateHttpSession(true)
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                        .logoutSuccessUrl("/login?logout")
+                                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Autowired
+    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
+       auth
+               .userDetailsService(userDetailsService)
+               .passwordEncoder(passwordEncoder());
+    }
 }
